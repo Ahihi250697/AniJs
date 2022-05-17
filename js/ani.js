3b51ee600a2f371b1d9af58358561677e062a725
Biihuy <biihuy25697@gmail.com>
2022-05-17 15:13:49 +0700

    upate

--- js/ani.js
+++ js/ani.js
@@ -1,97 +1,149 @@
-const Ani = (function (opts) {
-    const WindowHeight = $(window).innerHeight();
-    const AniClass = $(".ani");
+const AniJs = (function () {
+    const windowHeight = $(window).innerHeight(),
+        aniQuery = $(".ani"),
+        pageHeight = $("html").innerHeight();
 
-    const CreateWave = () => {
-        const Wave = $(".wave");
-        if (Wave.length < 1) return false;
+    let _defaults = {
+        delay: 150,
+        delayStart: 1000,
+    };
 
-        Wave.map((a, b) => {
-            let _h = $(b).text().trim().split ``;
-            _h = _h.map((v) => `<span>${v}</span>`).join("");
+    // ani wave
+    const InitWave = function () {
+        const _ = $(".wave");
+        if (_.length < 1) return false;
 
-            $(b).html(_h);
-        });
-    }
+        const CreateSpan = function (txt) {
+            let _return = "",
+                _txt = txt.split("");
 
-    const CreateType = () => {
-        const AniType = $(".type");
-        if (AniType.length < 1) return false;
+            for (let item of _txt) {
+                item !== " "
+                    ? (_return += `<span>${item}</span>`)
+                    : (_return += " ");
+            }
 
-        AniType.map((a, b) => {
-            let _target = $(b).find(".target");
-            let _d = -_defaults.timeDelay;
-            let _h = _target.text().trim().split ``;
-            _h = _h.map((v) => {
-                _d += _defaults.timeDelay;
-                return `<span>${v}</span>`;
-            }).join("");
+            return _return;
+        };
 
-            _target.html(_h);
-            _target.css({
-                'transition-duration': _d + 's'
-            })
-        });
+        for (let item of _) {
+            let _item = $(item).html().trim().replace(/\s\s+/g, ""),
+                _itemHasBr = _item.split("<br>"),
+                _html = "";
+
+            let _itemLength = _itemHasBr.length;
+            if (_itemLength !== 1) {
+                for (let _i = 0; _i < _itemLength; _i++) {
+                    _html += CreateSpan(_itemHasBr[_i]);
+
+                    if (_i !== _itemLength - 1) {
+                        _html += "<br>";
+                    }
+                }
+            } else {
+                _html = CreateSpan(_item);
+            }
+
+            $(item).html(_html);
+        }
+    };
+
+    // ani type
+    const InitImg = () => {
+        const _ = $(".img_slide");
+        if (_.length < 1) return false;
+
+        for (let item of _) {
+            let _item = $(item),
+                _children = _item.children();
+            if (_children.length === 1) {
+                _children.addClass("img__content");
+            } else {
+                _item.html(`<div class="img__content">${_item.html()}</div>`);
+            }
+        }
+    };
+
+    const InitLinebar = function () {
+        const _ = $(".linebar");
+        if (_.length < 1) return false;
+
+        for (let item of _) {
+            let _item = $(item),
+                _itemText = _item.text().trim();
+            _.html(`<span class="linebar__inside">${_itemText}</span>`);
+        }
     };
 
     // animation
-    const Animation = function (opt) {
+    const Animation = function () {
+        let // time delay each other
+            _time = 0,
+            // current offset
+            _rangeTop = window.pageYOffset,
+            // range show
+            _rangeShowUp = _rangeTop + windowHeight * 0.75,
+            // bottom
+            _rangeBotton = _rangeTop + windowHeight;
 
-        let _time = 0, // time delay each other
-            _b = window.pageYOffset, // scroll top page
-            _c = _b + WindowHeight; // scroll end page
+        if (_rangeBotton >= pageHeight - 50) {
+            _rangeShowup = _rangeBotton;
+        }
 
-        $.each(AniClass, function (a, b) {
-            let _ = $(b);
+        for (let item of aniQuery) {
+            const _ = $(item);
 
-            if (!_.hasClass("ani-passed")) {
+            if (!_.hasClass("ani-pass")) {
                 let _top = _.offset().top;
 
-                if (_top >= _b && _top <= _c) {
-                    // add class to check it is ready to show
-                    _.addClass("ani-passed");
+                if (_top >= _rangeTop && _top <= _rangeShowUp) {
+                    // add class to check if it will be show
+                    _.addClass("ani-pass");
+                    // set time out to add class 'animated'
+                    let _timeDelay = _time;
+                    if (_.attr("data-ani-delay")) {
+                        _timeDelay = +_.attr("data-ani-delay");
+                    } else {
+                        _time += _defaults.delay;
+                    }
 
-                    // set time out to add class animated
                     let _set = setTimeout(function () {
                         _.addClass("animated");
-                    }, _time);
-                    _time += opt.timeDelay;
-
-                } else if (_.offset().top < _b) {
-                    // add class if it is on top screen position
-                    _.addClass("ani-passed animated");
+                    }, _timeDelay);
+                } else if (_.offset().top < _rangeTop) {
+                    // add class if it is on top screen
+                    _.addClass("ani-pass animated");
                 }
             }
+        }
+    };
+
+    const Init = function (opts) {
+        _defaults = $.extend({}, _defaults, opts);
+        console.log(_defaults);
+        //create wave
+        InitWave();
+        //linebar
+        InitLinebar();
+        //create sparkle
+        InitImg();
+
+        // console.log(_defaults, opts);
+        $(window).on("load", function () {
+            setTimeout(function () {
+                Animation();
+            }, _defaults.delayStart);
+        });
+
+        $(window).on("scroll", function () {
+            Animation();
+            console.log("first");
         });
     };
 
     return {
-        init(opts = {}) {
-            // CreateWave(_opts);
-            // CreateType(_opts);
-            let _defaults = {
-                timeDelay: 150,
-                startAfter: 1000
-            }
-            _defaults = {
-                ..._defaults,
-                ...opts
-            };
-
-            $(window).on("load", function () {
-                setTimeout(function () {
-                    Animation(_defaults);
-                }, _defaults.startAfter);
-            });
-            $(window).on("scroll", function () {
-                setTimeout(function () {
-                    Animation(_defaults);
-                }, 50);
-            });
-        }
-    }
+        init: function (opts) {
+            Init(opts);
+        },
+    };
 })();
-
-Ani.init(
-
-);
